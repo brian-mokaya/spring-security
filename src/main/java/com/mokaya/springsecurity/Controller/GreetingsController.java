4d5421e28289a7af4fbb721c7bf8b082e751b185
brian-mokaya <brianmokaya@gmail.com>
2025-09-25 16:40:54 +0300

    Add security configuration and greeting endpoint
    - Implemented SecurityConfig for basic authentication and user management.
    - Added a new GreetingsController with a /hello endpoint returning "Hello World!".
    - Updated pom.xml to include spring-boot-starter-security and spring-security-test dependencies.
    - Changed project description in pom.xml.
    - Updated Java version in pom.xml to 23.

--- src/main/java/com/mokaya/springsecurity/Controller/GreetingsController.java
+++ src/main/java/com/mokaya/springsecurity/Controller/GreetingsController.java
@@ -1,4 +1,13 @@
 package com.mokaya.springsecurity.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
 }
---
+++ src/main/java/com/mokaya/springsecurity/configs/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.mokaya.springsecurity.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session
+                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user2")
+                .password("{noop}password2")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("User1")
+                .password("{noop}password2")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
