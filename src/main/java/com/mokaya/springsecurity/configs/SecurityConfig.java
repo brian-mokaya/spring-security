b8e7737a82b74bd2d22fae04c8ef6ff38bc7ea37
brian-mokaya <brianmokaya@gmail.com>
2025-09-26 15:43:58 +0300

    Add H2 database configuration and update security settings

--- src/main/java/com/mokaya/springsecurity/configs/SecurityConfig.java
+++ src/main/java/com/mokaya/springsecurity/configs/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.mokaya.springsecurity.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,20 +25,27 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated());
+
         http.sessionManagement(session
                 -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.withUsername("user2")
-                .password("{noop}password2")
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}password1")
                 .roles("USER")
                 .build();
 
@@ -40,7 +53,11 @@ public class SecurityConfig {
                 .password("{noop}password2")
                 .roles("ADMIN")
                 .build();
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        return new InMemoryUserDetailsManager(user1, user2);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(user2);
 
-        return new InMemoryUserDetailsManager(user1, user2);
+        return userDetailsManager;
     }
 }
