27966f8b92988daba6a64cec20fabeb6d6e2514b
brian-mokaya <brianmokaya@gmail.com>
2025-10-01 22:09:42 +0300

    Implement password encoding in SecurityConfig and add PasswordEncoder bean

--- src/main/java/com/mokaya/springsecurity/configs/SecurityConfig.java
+++ src/main/java/com/mokaya/springsecurity/configs/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -45,12 +47,12 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
+                .password(passwordEncoder().encode("password1"))
                 .roles("USER")
                 .build();
 
-        UserDetails user2 = User.withUsername("User1")
-                .password("{noop}password2")
+        UserDetails user2 = User.withUsername("User2")
+                .password(passwordEncoder().encode("password2"))
                 .roles("ADMIN")
                 .build();
 
@@ -60,5 +62,10 @@ public class SecurityConfig {
         userDetailsManager.createUser(user2);
 
         return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
